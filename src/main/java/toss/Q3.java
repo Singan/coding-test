40f8a3f042cee7ef8c9a229e46c9c4f3a9dab8b4
석성희 <sunghee119z@naver.com>
2023-10-28 02:59:42 +0900

    코딩테스트 문제 인잇

---
+++ src/main/java/toss/Q3.java
@@ -0,0 +1,9 @@
+package toss;
+
+public class Q3 {
+    //숫자로만 이루어진 문자열 s가 있습니다. (0 <=  s.length < 1,000)
+    //아래의 조건을 모두 만족하는 숫자를 '멋쟁이 숫자'라고 합니다.
+    public static void main(String[] args) {
+
+    }
+}
