0a2f439af47fc3c9bfb067a23681768f58c115c1
sunghee119z@naver.com <sunghee119z@naver.com>
2023-10-29 05:50:24 +0900

    앞으로는 무조건 버퍼더리더를 사용하자

--- src/main/java/basic1/Main.java
+++ src/main/java/basic1/Main.java
@@ -1,44 +1,44 @@
 package basic1;
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
     static Stack<Integer> stack = new Stack<>();
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int count = Integer.parseInt(scanner.nextLine());
+    public static void main (String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
         for (int i = 0; i < count; i++) {
-            String s[] = scanner.nextLine().split(" ");
-            System.out.println(i+" : " +s[0]);
-            switch (s[0]) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String s = st.nextToken();
+            switch (s) {
                 case "push":
-                    stack.push(Integer.parseInt(s[1]));
+                    stack.push(Integer.parseInt(st.nextToken()));
                     break;
                 case "top":
-                    //System.out.println(stack.size() == 0 ? -1:stack.get(stack.size() - 1));
+                    System.out.println(stack.size() == 0 ? -1:stack.get(stack.size() - 1));
                     break;
                 case "size":
-                   // System.out.println(stack.size());
+                    System.out.println(stack.size());
                     break;
                 case "pop":
                     if(stack.size() == 0 ){
-                     //   System.out.println(-1);
+                     System.out.println(-1);
                         break;
                     }
-                    //System.out.println(stack.get(stack.size() - 1));
+                    System.out.println(stack.get(stack.size() - 1));
                     stack.remove(stack.size() - 1);
                     break;
                 case "empty":
-                   // System.out.println(stack.isEmpty()?1:0);
+                    System.out.println(stack.isEmpty()?1:0);
                     break;
             }
         }
-
-
+        br.close();
     }
-
-
 }
--- src/main/java/basic1/StackExam.java
+++ src/main/java/basic1/StackExam.java
@@ -1,8 +1,8 @@
 package basic1;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class StackExam {
     //정수를 저장하는 스택을 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
@@ -12,48 +12,41 @@ public class StackExam {
     //size: 스택에 들어있는 정수의 개수를 출력한다.
     //empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
     //top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-    static List<Integer> stack = new ArrayList<>();
-    public static void main(String[] args) {
-        solution();
-    }
+    static Stack<Integer> stack = new Stack<>();
 
-    public static void solution(){
-        Scanner scanner = new Scanner(System.in);
-        int count = scanner.nextInt();
-
-        for (int i=0;i<count;i++){
-            switch (scanner.next()){
+    public static void main (String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
+        for (int i = 0; i < count; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String s = st.nextToken();
+            switch (s) {
                 case "push":
-                    stack.add(Integer.parseInt(scanner.next()));
+                    stack.push(Integer.parseInt(st.nextToken()));
                     break;
                 case "top":
-                    if(stack.size()==0){
-                        System.out.println(-1);
-                        break;
-                    }                    System.out.println(stack.get(stack.size()-1));;
+                    System.out.println(stack.size() == 0 ? -1:stack.get(stack.size() - 1));
                     break;
                 case "size":
                     System.out.println(stack.size());
                     break;
                 case "pop":
-                    if(stack.size()==0){
+                    if(stack.size() == 0 ){
                         System.out.println(-1);
                         break;
                     }
-                    System.out.println(stack.get(stack.size()-1));
-                    stack.remove(stack.size()-1);
+                    System.out.println(stack.get(stack.size() - 1));
+                    stack.remove(stack.size() - 1);
                     break;
                 case "empty":
-                    if(stack.size()==0){
-                        System.out.println(1);
-                        break;
-                    }else{
-                        System.out.println(0);
-                        break;
-                    }
+                    System.out.println(stack.isEmpty()?1:0);
+                    break;
             }
         }
-
-
+        br.close();
     }
+    //풀이 후기
+    // 처음에 리스트로 했으나(스택이란 자료구조 자체를 구현하는 문제로 생각) 고침
+    // 계속 시간초과가 뜨길래 스캐너에 nextLine 특성 상 마지막 입력에 개행문자가 없으면 엔터를 한번 더 쳐줘야 해서 그런줄 암.
+    // 알고보니 그냥 버퍼더리더를 쓰면 됨
 }
