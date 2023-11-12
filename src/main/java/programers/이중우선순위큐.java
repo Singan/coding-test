0f233d810cccbde35eba08de98faebdb25b62f46
sunghee119z@naver.com <sunghee119z@naver.com>
2023-11-13 00:31:47 +0900

    Update 이중우선순위큐.java

--- src/main/java/programers/이중우선순위큐.java
+++ src/main/java/programers/이중우선순위큐.java
@@ -1,4 +1,12 @@
 package programers;
 
 public class 이중우선순위큐 {
+
+    public static void main(String[] args) {
+
+    }
+    public static int[] solution(String[] operations) {
+        int[] answer = {};
+        return answer;
+    }
 }
