3275f31d7da7c56649baedc12b992f1d24b7c44f
sunghee119z@naver.com <sunghee119z@naver.com>
2023-11-14 00:10:03 +0900

    이중우선순위 큐 해결

--- src/main/java/programers/이중우선순위큐.java
+++ src/main/java/programers/이중우선순위큐.java
@@ -1,26 +1,42 @@
 package programers;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class 이중우선순위큐 {
 
     public static void main(String[] args) {
 
     }
+
     public static int[] solution(String[] operations) {
-        int[] answer = new int[operations.length];
+        int[] answer = new int[2];
+        List<Integer> answerList = new ArrayList<>();
         int minIndex = 0;
         int maxIndex = 0;
         for (int i = 0; i < operations.length; i++) {
             String[] s = operations[i].split(" ");
             String op = s[0];
             int number = Integer.parseInt(s[1]);
-            if(op.equals("I")){
-                minIndex = number<answer[minIndex]? i:minIndex;
-                maxIndex = number>answer[maxIndex]? i:maxIndex;
-            }else if(op.equals("D")){
-                
+            if (op.equals("I")) {
+                answerList.add(number);
+            } else if (op.equals("D")) {
+                if (answerList.size() > 0) {
+                    int delete = 0;
+                    if (number == -1) {
+                        delete = Collections.min(answerList);
+                    } else {
+                        delete = Collections.max(answerList);
+                    }
+                    answerList.remove(answerList.indexOf(delete));
+                }
             }
         }
-
+        if (answerList.size() != 0) {
+            answer[1] = Collections.min(answerList);
+            answer[0] = Collections.max(answerList);
+        }
         return answer;
     }
 }
