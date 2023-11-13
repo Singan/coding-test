1c5c179a9d0bf6ec132a03eb59b38720ad5eb121
sunghee119z@naver.com <sunghee119z@naver.com>
2023-11-13 23:56:55 +0900

    Update 이중우선순위큐.java

--- src/main/java/programers/이중우선순위큐.java
+++ src/main/java/programers/이중우선순위큐.java
@@ -6,7 +6,21 @@ public class 이중우선순위큐 {
 
     }
     public static int[] solution(String[] operations) {
-        int[] answer = {};
+        int[] answer = new int[operations.length];
+        int minIndex = 0;
+        int maxIndex = 0;
+        for (int i = 0; i < operations.length; i++) {
+            String[] s = operations[i].split(" ");
+            String op = s[0];
+            int number = Integer.parseInt(s[1]);
+            if(op.equals("I")){
+                minIndex = number<answer[minIndex]? i:minIndex;
+                maxIndex = number>answer[maxIndex]? i:maxIndex;
+            }else if(op.equals("D")){
+                
+            }
+        }
+
         return answer;
     }
 }
