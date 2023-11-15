fc196e6bdf7bd8208b72c864940d561c5a953b27
sunghee119z@naver.com <sunghee119z@naver.com>
2023-11-15 22:06:23 +0900

    Create 단속카메라.java

---
+++ src/main/java/programers/단속카메라.java
@@ -0,0 +1,13 @@
+package programers;
+
+public class 단속카메라 {
+    public static void main(String[] args) {
+
+    }
+    private static class Solution {
+        public int solution(int[][] routes) {
+            int answer = 0;
+            return answer;
+        }
+    }
+}
