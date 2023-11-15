ee07be17410db819ab4b5c6fa6db2c33d05e7c6e
sunghee119z@naver.com <sunghee119z@naver.com>
2023-11-16 02:24:53 +0900

    Update 단속카메라.java

--- src/main/java/programers/단속카메라.java
+++ src/main/java/programers/단속카메라.java
@@ -1,5 +1,7 @@
 package programers;
 
+import java.util.Arrays;
+
 public class 단속카메라 {
     public static void main(String[] args) {
 
@@ -7,10 +9,18 @@ public class 단속카메라 {
     private static class Solution {
         public int solution(int[][] routes) {
 
-            
-
-
             int answer = 0;
+            int max =-30001;
+            Arrays.sort(routes,(o1, o2)->o1[1]-o2[1]);
+            for(int i = 0 ; i<routes.length;i++){
+                if(routes[i][0]>max){
+                    max = routes[i][1];
+                    answer++;
+                }
+            }
+
+
+
             return answer;
         }
     }
