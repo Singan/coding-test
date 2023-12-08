a09adbf6a445195aa624388b026f044deea78a19
석성희 <sunghee119z@naver.com>
2023-12-08 22:35:12 +0900

    삼각형과 세변

--- src/main/java/backjun/etc/삼각형과세변.java
+++ src/main/java/backjun/etc/삼각형과세변.java
@@ -1,7 +1,40 @@
 package backjun.etc;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class 삼각형과세변 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        String[] strings = {
+                "Scalene", "Isosceles ", "Equilateral", "Invalid"
+        };
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        while (scanner.hasNextLine()) {
+            int max = Arrays.stream(new int[]{a, b, c}).max().getAsInt();
+            int sum = a + b + c - max;
+            int chk = 0;
+            if (a + b + c == 0)
+                break;
+
+            if (a == b) {
+                chk++;
+                if (b == c) {
+                    chk++;
+                }
+            }else if (b == c || a==c) {
+                chk++;
+            }
+            if (sum <= max) {
+                chk = 3;
+            }
+            System.out.println(strings[chk]);
+            a = scanner.nextInt();
+            b = scanner.nextInt();
+            c = scanner.nextInt();
+        }
     }
 }
