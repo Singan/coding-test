aca5d58b195cc195192789d144d5a9289d81ab45
sunghee119z@naver.com <sunghee119z@naver.com>
2024-01-27 23:39:47 +0900

    Create 가장받이받은선물.java

---
+++ src/main/java/programers/가장받이받은선물.java
@@ -0,0 +1,11 @@
+package programers;
+
+public class 가장받이받은선물 {
+    //https://school.programmers.co.kr/learn/courses/30/lessons/258712
+    class Solution {
+        public int solution(String[] friends, String[] gifts) {
+            int answer = 0;
+            return answer;
+        }
+    }
+}
