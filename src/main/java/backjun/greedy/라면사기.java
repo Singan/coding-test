a37cac6a787b2d9d23d0a7cc0202f0ea1110ddbe
sunghee119z@naver.com <sunghee119z@naver.com>
2024-02-19 23:48:23 +0900

    Update 라면사기.java

--- src/main/java/backjun/greedy/라면사기.java
+++ src/main/java/backjun/greedy/라면사기.java
@@ -1,7 +1,13 @@
 package backjun.greedy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class 라면사기 {
-    //라면매니아 교준이네 집 주변에는 N개의 라면 공장이 있다. 각 공장은 1번부터 N번까지 차례대로 번호가 부여되어 있다. 교준이는 i번 공장에서 정확하게 Ai개의 라면을 구매하고자 한다(1 ≤ i ≤ N).
+    //라면매니아 교준이네 집 주변에는 N개의 라면 공장이 있다.
+    // 각 공장은 1번부터 N번까지 차례대로 번호가 부여되어 있다. 교준이는 i번 공장에서 정확하게 Ai개의 라면을 구매하고자 한다(1 ≤ i ≤ N).
     //
     //교준이는 아래의 세 가지 방법으로 라면을 구매할 수 있다.
     //
@@ -14,7 +20,45 @@ public class 라면사기 {
     //첫 번째 줄에 라면 공장의 개수를 의미하는 자연수 N가 주어진다.
     //
     //두번째 줄에 N개의 정수 A1, ···, AN가 사이에 공백을 두고 주어진다.
-    public static void main(String[] args) {
+    //모든 입력 데이터는 다음 조건을 만족한다.
+    //
+    //3 ≤ N ≤ 104
+    //0 ≤ Ai ≤ 104 (1 ≤ i ≤ N)
+    static int[] factory;
+    static int value[] = new int[]{3, 2, 2};
+    static int n;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        factory = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            factory[i] = Integer.parseInt(st.nextToken());
+        }
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            if (factory[i] > 0) {
+                result +=  nextFactory(i, 0, 10 * 10 * 10 * 10 * 10);
+                System.out.println(result);
+            }
+        }
+        System.out.println(result);
     }
+
+    public static int nextFactory(int i, int idx, int count) {
+        if (i+idx >= n || idx >= value.length || factory[i + idx] == 0)
+            return 0;
+
+        count = Math.min(factory[i + idx], count);
+        System.out.println(i +"  : " + count);
+        factory[i + idx] = factory[i + idx] - count;
+        int money = count * value[idx];
+        money += nextFactory(i, idx + 1, count);
+
+        return money;
+    }
+
 }
