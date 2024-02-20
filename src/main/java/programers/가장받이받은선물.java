606c3a6b3da980a635ce4d2f1386aefe60eddd88
sunghee119z@naver.com <sunghee119z@naver.com>
2024-02-20 23:59:22 +0900

    Update 가장받이받은선물.java

--- src/main/java/programers/가장받이받은선물.java
+++ src/main/java/programers/가장받이받은선물.java
@@ -2,7 +2,7 @@ package programers;
 
 public class 가장받이받은선물 {
     //https://school.programmers.co.kr/learn/courses/30/lessons/258712
-    class Solution {
+    static class Solution {
         public int solution(String[] friends, String[] gifts) {
             int answer = 0;
             return answer;
