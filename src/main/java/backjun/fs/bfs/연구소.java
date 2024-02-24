f5748ac6b316f3a5e2efb87cb4bd358e3b32028a
sunghee119z@naver.com <sunghee119z@naver.com>
2024-02-24 23:38:50 +0900

    Create 연구소.java

---
+++ src/main/java/backjun/fs/bfs/연구소.java
@@ -0,0 +1,25 @@
+package backjun.fs.bfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 연구소 {
+    //https://www.acmicpc.net/problem/14502
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        final int N = Integer.parseInt(st.nextToken());
+        final int M = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[N][M];
+
+
+
+
+
+    }
+}
