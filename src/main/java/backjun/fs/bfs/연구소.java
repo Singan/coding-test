7de30b6e34dc090525d3e03ec1d277737d0dedeb
sunghee119z@naver.com <sunghee119z@naver.com>
2024-02-25 10:13:58 +0900

    Update 연구소.java

--- src/main/java/backjun/fs/bfs/연구소.java
+++ src/main/java/backjun/fs/bfs/연구소.java
@@ -3,23 +3,87 @@ package backjun.fs.bfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class 연구소 {
     //https://www.acmicpc.net/problem/14502
+    static int[][] arr;
+    static int[][] move = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    static boolean[][] visit;
+    static int N, M;
+    static int result = Integer.MIN_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        final int N = Integer.parseInt(st.nextToken());
-        final int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[N][M];
+        arr = new int[N][M];
+        visit = new boolean[N][M];
 
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        dfs(0);
 
+    }
 
+    public static void dfs(int depth) {
+        if (depth == 3) {
+            bfs();
+            return;
+        }
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < M; j++) {
+                if (arr[i][j] == 0) {
+                    arr[i][j] = 1;
+                    dfs(depth + 1);
+                    arr[i][j] = 0;
+                }
+            }
+        }
+    }
 
+    public static void bfs() {
+        Queue<int[]> queue = new LinkedList<>();
+        int[][] virusMap = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                virusMap[i][j] = arr[i][j];
+                if (virusMap[i][j] == 2) {
+                    queue.add(new int[]{i, j});
+                }
+            }
+        }
 
+        while (!queue.isEmpty()) {
+            int cur[] = queue.poll();
+            int curX = cur[0];
+            int curY = cur[1];
+            for (int[] m : move) {
+                int x = curX + m[0];
+                int y = curY + m[1];
+                if (x < N && y < M && x >= 0 && y >= 0 && virusMap[x][y] == 0) {
+                    virusMap[x][y] = 2;
+                }
+            }
+
+        }
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (virusMap[i][j] == 0) {
+                    count++;
+                }
+            }
+        }
+        result = Math.max(result, count);
     }
 }
