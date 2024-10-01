eb65a62af3ff040f34825a5fcec2021673525790
석성희 <sunghee119z@naver.com>
2024-10-01 09:57:45 +0900

    [Gold IV] Title: 파일 합치기 3, Time: 2568 ms, Memory: 279400 KB -BaekjoonHub

---
+++ 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+// 입력 : 테스트케이스
+// 파일 수
+// 파일 크기 . . .
+// 파일 크기가 작은 것을 우선으로 계속 더하면 되는 그리디 문제 같다.
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            int k = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            PriorityQueue<Long> queue = new PriorityQueue<>();
+            for (int i = 0; i < k; i++) {
+                queue.add(Long.parseLong(st.nextToken()));
+            }
+
+            long result = 0; // k 는 최소 3이다.
+            while (!queue.isEmpty()){
+                if(queue.size() <= 1)
+                    break;
+                long next = queue.poll() + queue.poll();
+
+                queue.add(next);
+                result+= next;
+            }
+
+            System.out.println(result);
+        }
+    }
+}
