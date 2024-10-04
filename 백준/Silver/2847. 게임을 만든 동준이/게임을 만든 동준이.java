2a74753afdef7bfaf626f0f75a1797a0f4ec5445
석성희 <sunghee119z@naver.com>
2024-10-04 22:20:32 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 100 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.java
@@ -1,8 +1,9 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 //https://www.acmicpc.net/problem/2847
+// 마지막 값은 항상 최고값이여한다.
+// 마지막 이전 값은 마지막 값보다 1 작아야 한다.
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,10 +15,11 @@ public class Main {
         }
         int count = 0;
         for (int i = n - 2; i >= 0; i--) {
-            if(arr[i] >= arr[i+1]){
-                arr[i]--;
-                i++;
-                count++;
+            if (arr[i] >= arr[i + 1]) {
+                int dif = arr[i] - (arr[i] - arr[i + 1] + 1);
+                count += arr[i] - dif;
+                arr[i] = dif;
+
             }
         }
         System.out.println(count);
