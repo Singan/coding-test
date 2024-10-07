e894d3a676923930aa650a2e3abe68f979752fce
석성희 <sunghee119z@naver.com>
2024-10-07 16:22:32 +0900

    [Gold IV] Title: 키 순서, Time: 616 ms, Memory: 50604 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -1,68 +1,75 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
-//https://www.acmicpc.net/problem/2458
-//모든 노드와 연결된 노드를 찾는 문제
-// 단방향으로 구현해야 상위 노드에서 하위 노드로 갔다가 다시 상위 노드로 가는 상황이 안나온다.
-// 대신 상위 노드에서 하위 노드 탐색 시 하위노드에 상위 노드를 체크해줘야 한다.
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str[] = br.readLine().split(" ");
+        String[] str = br.readLine().split(" ");
 
-        int n = Integer.parseInt(str[0]); //사람의 수 (배열의 크기)
+        int n = Integer.parseInt(str[0]); // 사람의 수 (배열의 크기)
         int m = Integer.parseInt(str[1]); // 키의 비교 횟수 (br 반복문)
 
-        boolean[][] arr = new boolean[n + 1][n + 1];
-        boolean[][] chk = new boolean[n + 1][n + 1];
+        boolean[][] arr = new boolean[n + 1][n + 1]; // 키 비교 정보를 저장할 배열
+
+        // 키 비교 정보 입력
         for (int i = 0; i < m; i++) {
-            String stringArr[] = br.readLine().split(" ");
+            String[] stringArr = br.readLine().split(" ");
             int a = Integer.parseInt(stringArr[0]);
             int b = Integer.parseInt(stringArr[1]);
-            arr[a][b] = true;
+            arr[a][b] = true; // a가 b보다 키가 작다
         }
-        for (int i = 1; i < arr.length; i++) {
+
+        // 각 사람마다 BFS를 통해 자신의 키가 비교 가능한 사람들을 확인
+        boolean[][] chk = new boolean[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
             chk[i] = bfs(i, arr);
         }
-        for (int i = 0; i <chk.length ; i++) {
-            for (int j = 0; j < chk.length ; j++) {
-                if(chk[i][j]){
-                    chk[j][i] = true;
+
+        // 위상관계를 상호 반영
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (chk[i][j]) {
+                    chk[j][i] = true; // 상위 관계를 반영
                 }
             }
         }
 
+        // 모든 사람과의 키 비교가 가능한 사람을 카운트
         int count = 0;
-        for (int i = 1; i < chk.length; i++) {
+        for (int i = 1; i <= n; i++) {
             boolean flag = true;
-            for (int j = 1; j < chk.length; j++) {
-                if(!chk[i][j])
+            for (int j = 1; j <= n; j++) {
+                if (i != j && !chk[i][j] && !chk[j][i]) {
                     flag = false;
+                    break;
+                }
             }
-            if(flag)
+            if (flag) {
                 count++;
+            }
         }
         System.out.println(count);
     }
 
+    // BFS를 통해 각 사람의 키 비교 관계를 탐색하는 함수
     static boolean[] bfs(int me, boolean[][] arr) {
-        Queue<Integer> queue = new LinkedList<>(); // 다음 노드의 위치와 몇 번째인지를 카운트
-        boolean visit[] = new boolean[arr.length]; // 이미 친구인 친구를 다시 탐색하지 않도록
-        visit[me] = true; // 본인 탐색은 막는다.
+        Queue<Integer> queue = new LinkedList<>(); // 탐색을 위한 큐
+        boolean[] visit = new boolean[arr.length]; // 방문 여부 체크 배열
+        visit[me] = true; // 자기 자신은 탐색하지 않음
         queue.add(me);
-        while (!queue.isEmpty()) {
-            int cur = queue.poll();
 
+        while (!queue.isEmpty()) {
+            int cur = queue.poll(); // 현재 노드
+
+            // 연결된 노드를 탐색
             for (int i = 1; i < arr.length; i++) {
-                if (arr[cur][i] && !visit[i]) {
+                if (arr[cur][i] && !visit[i]) { // cur보다 키가 작은 사람 탐색
                     queue.add(i);
                     visit[i] = true;
                 }
             }
-
         }
         return visit;
     }
