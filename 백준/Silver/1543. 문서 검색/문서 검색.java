628087c37d9fc2e1eed38f974cfc60d5909060bb
석성희 <sunghee119z@naver.com>
2024-10-08 10:06:52 +0900

    [Silver V] Title: 문서 검색, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.java
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -1,23 +1,22 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
         String subStr = br.readLine();
 
         int count = 0;
-        int index = 0;
+        int index = str.indexOf(subStr, 0);
 
-        // 문서에서 더 이상 찾을 수 없을 때까지 반복
-        while ((index = str.indexOf(subStr, index)) != -1) {
+
+        while (index != -1) {
+            index = str.indexOf(subStr, index + subStr.length());
             count++;
-            index += subStr.length();
         }
-
         System.out.println(count);
     }
 }
