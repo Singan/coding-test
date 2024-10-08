b6506d292a3cde68b3d5e1deb231e57b3a00113c
석성희 <sunghee119z@naver.com>
2024-10-08 12:50:34 +0900

    [Gold III] Title: 게임 닉네임, Time: 444 ms, Memory: 76112 KB -BaekjoonHub

--- 백준/Gold/16934. 게임 닉네임/게임 닉네임.java
+++ 백준/Gold/16934. 게임 닉네임/게임 닉네임.java
@@ -1,80 +1,126 @@
-
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
+// 게임 닉네임
+// 트라이 문제 
 public class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.valueOf(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		Trie16934 trie = new Trie16934();
+		
+		for(int i = 0; i < n; i++) {
+			String word = br.readLine();
+			int aliasIndex = trie.insertAndGetAliasIndex(word);	// 삽입한 다음 같은 접두사의 끝위치 인덱스 리턴 
+			if(aliasIndex == -1) {	// 완전히 일치하는 단어가 존재하면 
+				int count = trie.getSameCount(word);
+				
+				if(count == 1) {	// 같은 단어가 1개만 존재하는 경우	 
+					sb.append(word);
+				}
+				if(count > 1) {
+					sb.append(word).append(count);
+				}
+			}
+			else {
+				sb.append(word.substring(0, aliasIndex));
+			}
+			
+			sb.append("\n");
+		}
+		
+		System.out.println(sb.toString());
+	}
+}
 
+//트라이 알고리즘에 사용되는 노드 
+class TrieNode16934 {
+	private Map<Character, TrieNode16934> childNode = new HashMap<>();
+	private boolean isLastChar;
+	private int count;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Trie trie = new Trie();
-        int n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
-            String str = br.readLine();
-            StringBuilder sb = new StringBuilder(str);
-            int prefixStart = trie.findPrefix(str);
-            trie.insert(str);
-            if (prefixStart + 1 >= str.length()) {
-                int count = trie.searchCount(str);
-                if (count > 1) {
-                    sb.append(count);
-                }
-                System.out.println(sb);
-            } else {
-                System.out.println(sb.substring(0, prefixStart + 1));
-            }
+	// 해당 문자가 마지막 문자인지
+	public boolean isLastChar() {
+		return this.isLastChar;
+	}
 
-        }
-    }
+	public int getCount() {
+		return count;
+	}
+	
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	public void setLastChar(boolean isLastChar) {
+		this.isLastChar = isLastChar;
+	}
 
+	public Map<Character, TrieNode16934> getChildNodes() {
+		return this.childNode;
+	}
+	
+	public TrieNode16934 makeChildNodes(char ch) {
+		this.childNode.put(ch, new TrieNode16934());
+		return this.childNode.get(ch);
+	}
+}
 
-    static class TrieNode {
-        Map<Character, TrieNode> children;
-        boolean isEndOfWord;
+//트라이 알고리즘 
+class Trie16934 {
+	public TrieNode16934 rootTemp;
 
-        public TrieNode() {
-            children = new HashMap<>();
-            isEndOfWord = false;
-        }
-    }
+	// 생성자 생성
+	Trie16934() {
+		this.rootTemp = new TrieNode16934();
+	}
 
-    static class Trie {
-        private TrieNode root = new TrieNode();
-        private Map<String, Integer> countMap = new HashMap<>();
+	// 삽입 및 별칭을 만들 수 있는 인덱스 위치 리턴
+	// -1 리턴 : 똑같은 단어가 기존에 존재 
+	public int insertAndGetAliasIndex(String word) {
+		TrieNode16934 tempNode = this.rootTemp;
+		int index = -1;
+		boolean isEnd = false;
+		
+		for(int i = 0; i < word.length(); i++) {
+			// 이미 기존에 i번째 단어까지 접두사가 같은 경우 
+			if(tempNode.getChildNodes().containsKey(word.charAt(i))) {
+				tempNode = tempNode.getChildNodes().get(word.charAt(i));
+			}
+			else {	// 같은 접두사가 끝나버린 경우 
+				if(!isEnd) {
+					index = i + 1;
+					isEnd = true;
+				}
+			
+				tempNode = tempNode.makeChildNodes(word.charAt(i));
+			}
+		}
+	
+		if(index != -1) {	// 같은 접두사가 없는 경우 맨 끝 노드 count 1로 설정 
+			tempNode.setCount(1);
+		}
+		else {
+			tempNode.setCount(tempNode.getCount() + 1);
+		}
+		
+		return index;
+	}
 
-        public void insert(String str) {
-            int value = countMap.getOrDefault(str, 0);
-            countMap.put(str, value + 1);
-
-            TrieNode node = root;
-            for (char c : str.toCharArray()) {
-                node.children.putIfAbsent(c, new TrieNode());
-                node = node.children.get(c); // 자식을 계속 찾아야한다.
-            }
-            node.isEndOfWord = true;
-
-        }
-
-        public int searchCount(String str) {
-            int value = countMap.getOrDefault(str, 0);
-            return value;
-        }
-
-        public int findPrefix(String str) {
-            TrieNode node = root;
-            int n = 0;
-            for (char c : str.toCharArray()) {
-                node = node.children.get(c);
-
-                if (node == null) {
-                    break;
-                }
-                n++;
-            }
-            return n;
-        }
-
-    }
+	// 같은 단어가 존재할 때, 같은 단어의 개수 가져오기 
+	public int getSameCount(String word) {
+		TrieNode16934 tempNode = this.rootTemp;
+		
+		for(int i = 0; i < word.length(); i++) {
+			char ch = word.charAt(i);
+			TrieNode16934 node = tempNode.getChildNodes().get(ch); 
+			tempNode = node;
+		}
+	  	
+		return tempNode.getCount();
+	}
 }
