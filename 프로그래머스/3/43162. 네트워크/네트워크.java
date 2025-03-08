e55a08e57e0275631576363c282c2d982a669390
석성희 <sunghee119z@naver.com>
2025-03-09 01:54:31 +0900

    [level 3] Title: 네트워크, Time: 1.76 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        boolean [][] chk = new boolean[n][n];
+        for(int i = 0 ;i<n;i++){
+            for(int j = i; j<n;j++){
+                if(!chk[i][j]&&computers[i][j]==1){
+                    chk[i][j] = true;
+                    chk[j][i] = true;
+                }
+            }
+        }
+        Queue<Integer> queue = new LinkedList();
+        for(int i = 0 ;i<n;i++){
+            for(int j = 0; j<n;j++){
+                if(chk[i][j]){
+                    queue.offer(j);
+                    while(!queue.isEmpty()){
+                        int poll = queue.poll();
+                        for(int h=0;h<computers[poll].length;h++){
+                            if(chk[poll][h]){
+                                chk[poll][h] = false;
+                                chk[h][poll] = false;
+                                queue.offer(h);
+                            }
+                        }
+                    }
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
