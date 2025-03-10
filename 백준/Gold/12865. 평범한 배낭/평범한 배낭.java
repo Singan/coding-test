2949a683c869d9c0bab6dc66bb9571857d16e681
석성희 <sunghee119z@naver.com>
2025-03-11 05:28:35 +0900

    [Gold V] Title: 평범한 배낭, Time: 136 ms, Memory: 15600 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -7,22 +7,29 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());  // 물건 개수
-        int k = Integer.parseInt(st.nextToken());  // 최대 무게
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        int[] dp = new int[k + 1];
+        int dp[] = new int[k + 1];
+
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            int weight = Integer.parseInt(st.nextToken()); // 물건의 무게
-            int value = Integer.parseInt(st.nextToken());  // 물건의 가치
+            int weight = Integer.parseInt(st.nextToken()); // 무게;
+            int value = Integer.parseInt(st.nextToken()); // 가치
 
-            // **뒤에서부터 업데이트해야 함! (무게 초과 방지)**
+            //dp[i]=max(dp[i],dp[i−weight]+value)
             for (int j = k; j >= weight; j--) {
                 dp[j] = Math.max(dp[j], dp[j - weight] + value);
             }
+
         }
 
-        System.out.println(dp[k]); // 최대 가치 출력
+
+        // 각 0번에 물건을 담은 무게를 담는다.
+
+
+        System.out.println(dp[k]);
+
     }
 }
