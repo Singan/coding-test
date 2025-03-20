eca5ac016b17d7181850f97ad9d0ecd25d17cbc6
석성희 <sunghee119z@naver.com>
2025-03-21 06:30:39 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 308 ms, Memory: 27504 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -14,23 +14,22 @@ public class Main {
         int board[][] = new int[n][m];
         int dp[][] = new int[n][m];
         int answer = 0;
-        for (int i = 0; i < n; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < m; j++) {
-                int x = line.charAt(j) - '0';
-                board[i][j] = x;
-                dp[i][j] = x;
-                answer = Math.max(answer, x); // 이 부분 수정!
-            }
-        }
+for (int i = 0; i < n; i++) {
+    String line = br.readLine();
+    for (int j = 0; j < m; j++) {
+        dp[i][j] = board[i][j] = line.charAt(j) - '0';
+        answer = Math.max(answer, dp[i][j]); // 동시에 answer 갱신
+    }
+}
         for (int i = 1; i < n; i++) {
             for (int j = 1; j < m; j++) {
                 if (board[i][j] == 1) {
                     dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
-                    answer = Math.max(answer, dp[i][j]);
                 }
+                answer = Math.max(answer, dp[i][j]);
             }
         }
         System.out.println(answer * answer);
     }
 }
+
