e56dcab9480c893ff4ff06c4da333b442b13955f
석성희 <sunghee119z@naver.com>
2025-03-30 04:32:50 +0900

    [Gold V] Title: 전깃줄, Time: 108 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.java
+++ 백준/Gold/2565. 전깃줄/전깃줄.java
@@ -11,28 +11,22 @@ public class Main {
         List<int[]> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            list.add(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+            list.add(new int[]{Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())});
         }
 
-        // A 전봇대 기준으로 정렬
-        Collections.sort(list, Comparator.comparingInt(o -> o[0]));
-
-        // LIS를 구하기 위한 DP 배열 초기화
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);  // 최소 부분 수열 길이는 1 (자기 자신)
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (list.get(j)[1] < list.get(i)[1]) {  // 증가하는 부분 수열 조건
+        Collections.sort(list , Comparator.comparingInt(o -> o[0]));
+        int dp[] = new int[n + 1];
+        Arrays.fill(dp,1);
+        int lis = 0;
+        for (int i = 1; i <n; i++) {
+            for (int j = 0; j <i; j++) {
+                if(list.get(i)[1] > list.get(j)[1]){
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
+            lis = Math.max(dp[i] , lis);
         }
-
-        // LIS의 최대 길이 계산
-        int lisLength = Arrays.stream(dp).max().orElse(1);
-
-        // 최소 제거 전깃줄 수: 전체 전깃줄 - LIS 길이
-        System.out.println(n - lisLength);
+        System.out.println(n - lis);
     }
+
 }
