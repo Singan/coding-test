1a310af33deac7f095a3f586b1c2a7c2d24f525c
석성희 <sunghee119z@naver.com>
2025-03-30 05:12:40 +0900

    [Gold V] Title: 내려가기, Time: 348 ms, Memory: 46584 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,33 +7,48 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int dp[][] = new int[n][3];
         int arr[][] = new int[n][3];
+        int[] dpMax = new int[3];
+        int[] dpMin = new int[3];
 
+        // 입력 받기
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 3; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        dp[0] = arr[0];
-        int max = 0;
-        int min = Integer.MAX_VALUE;
 
+        // 첫 번째 행 dpMax, dpMin 초기화
+        dpMax[0] = arr[0][0];
+        dpMax[1] = arr[0][1];
+        dpMax[2] = arr[0][2];
+        dpMin[0] = arr[0][0];
+        dpMin[1] = arr[0][1];
+        dpMin[2] = arr[0][2];
 
+        // DP 테이블 갱신
         for (int i = 1; i < n; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0] , dp[i - 1][1]) + arr[i][0];
-            dp[i][1] = Math.max(dp[i - 1][0] , Math.max(dp[i - 1][1] , dp[i - 1][2])) + arr[i][1];
-            dp[i][2] = Math.max(dp[i - 1][2] , dp[i - 1][1]) + arr[i][2];
-        }
-        max = Math.max(dp[n - 1][0] , Math.max(dp[n - 1][1] , dp[n - 1][2]));
-        for (int i = 1; i < n; i++) {
-            dp[i][0] = Math.min(dp[i - 1][0] , dp[i - 1][1]) + arr[i][0];
-            dp[i][1] = Math.min(dp[i - 1][0] , Math.min(dp[i - 1][1] , dp[i - 1][2])) + arr[i][1];
-            dp[i][2] = Math.min(dp[i - 1][2] , dp[i - 1][1]) + arr[i][2];
+            int[] current = arr[i];
+
+            int prevMax0 = dpMax[0], prevMax1 = dpMax[1], prevMax2 = dpMax[2];
+            int prevMin0 = dpMin[0], prevMin1 = dpMin[1], prevMin2 = dpMin[2];
+
+            dpMax[0] = Math.max(prevMax0, prevMax1) + current[0];
+            dpMax[1] = Math.max(prevMax0, Math.max(prevMax1, prevMax2)) + current[1];
+            dpMax[2] = Math.max(prevMax2, prevMax1) + current[2];
+
+            dpMin[0] = Math.min(prevMin0, prevMin1) + current[0];
+            dpMin[1] = Math.min(prevMin0, Math.min(prevMin1, prevMin2)) + current[1];
+            dpMin[2] = Math.min(prevMin2, prevMin1) + current[2];
         }
 
-        min = Math.min(dp[n - 1][0] , Math.min(dp[n - 1][1] , dp[n - 1][2]));
+        // 최댓값과 최솟값 계산
+        int max = Math.max(dpMax[0], Math.max(dpMax[1], dpMax[2]));
+        int min = Math.min(dpMin[0], Math.min(dpMin[1], dpMin[2]));
+
+        // 결과 출력
         System.out.println(max + " " + min);
     }
 }
+
