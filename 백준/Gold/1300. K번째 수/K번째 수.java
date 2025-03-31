0a4bfca3e4290112ae95df560f0c25596dbc05db
석성희 <sunghee119z@naver.com>
2025-04-01 05:03:36 +0900

    [Gold I] Title: K번째 수, Time: 208 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.java
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -1,4 +1,5 @@
-
+import java.io.BufferedReader;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,27 +9,25 @@ public class Main {
         long n = sc.nextLong();  // n을 long으로 변경
         long k = sc.nextLong();  // k를 long으로 변경
 
-        long start = 1;  // 최소값은 1
-        long end = n * n;  // 최대값은 n * n
-        long result = 0;
+        long start = 1;
+        long end = n * n;
+
+        long result = 1;
 
         while (start <= end) {
             long mid = (start + end) / 2;
             long count = 0;
-
-            // mid 이하인 값들의 개수 세기
-            for (long i = 1; i <= n; i++) {
-                count += Math.min(mid / i, n);  // j의 최대값은 n
+            for (int i = 1; i <=n; i++) {
+                count += Math.min(mid / i , n);
             }
-
-            if (count < k) {
-                start = mid + 1;  // k보다 작으면 start를 올려야 함
-            } else {
-                end = mid - 1;  // k보다 크거나 같으면 end를 낮춰야 함
-                result = mid;  // 적당한 mid를 result에 저장
+            if(count < k){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+                result = mid;
             }
         }
-
         System.out.println(result);
+
     }
 }
