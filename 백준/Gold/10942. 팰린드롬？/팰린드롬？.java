fdf4d297edbdf41ba284a0f3f5487da60df93dae
석성희 <sunghee119z@naver.com>
2025-04-08 03:44:34 +0900

    [Gold IV] Title: 팰린드롬?, Time: 772 ms, Memory: 230600 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -0,0 +1,53 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int arr[] = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int question = Integer.parseInt(br.readLine());
+
+        int dp[][] = new int[n + 1][n + 1];
+
+        for (int len = 1; len <= n; len++) {
+            for (int i = 1; i <= n; i++) {
+
+
+                int j = (len + i) - 1;
+                if (j > n)
+                    continue;
+                if (len == 1) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                if (arr[i] == arr[j]) {
+                    if (len == 2) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+            }
+        }
+
+        StringBuffer sb = new StringBuffer();
+        while (question > 0) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            sb.append(dp[start][end]).append("\n");
+            question--;
+        }
+        System.out.println(sb);
+    }
+}
